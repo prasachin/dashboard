e1d1e3cd9b0023593af44229381c9a3af82e0e19
sachin <sachinp@iitbhilai.ac.in>
2024-05-27 14:37:38 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [value, setValue] = useState("");
+  // const [value, setValue] = useState("");
   const [data, setdata] = useState([]);
   // const [rates, setRates] = useState({})
   // const [currency, setCurrency] = useState(null);
@@ -26,35 +26,36 @@ const App = () => {
     // }
   }, []);
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
+  // const handleChange = (event) => {
+  //   setValue(event.target.value);
+  // };
 
-  const onSearch = (event) => {
-    event.preventDefault();
-    // setCurrency(value)
-    const filteredCountries = currency.filter((country) =>
-      country.name.common.toLowerCase().includes(value.toLowerCase())
-    );
+  // const onSearch = (event) => {
+  //   event.preventDefault();
+  //   // setCurrency(value)
+  //   const filteredCountries = currency.filter((country) =>
+  //     country.name.common.toLowerCase().includes(value.toLowerCase())
+  //   );
 
-    if (filteredCountries.length > 10) {
-      console.log(filteredCountries);
-      alert("Too many matches, please make your query more specific.");
-    } else {
-      // Handle the filtered countries, you might want to set them in state
-      console.log(filteredCountries);
-      setCurrency(filteredCountries);
-    }
-  };
+  //   if (filteredCountries.length > 10) {
+  //     console.log(filteredCountries);
+  //     alert("Too many matches, please make your query more specific.");
+  //   } else {
+  //     // Handle the filtered countries, you might want to set them in state
+  //     console.log(filteredCountries);
+  //     setCurrency(filteredCountries);
+  //   }
+  // };
 
   return (
     <div>
       {/* <form onSubmit={onSearch}> */}
-        {/* Find Countries : <input value={value} onChange={handleChange} /> */}
-        {/* <button type="submit">exchange rate</button> */}
+      {/* Find Countries : <input value={value} onChange={handleChange} /> */}
+      {/* <button type="submit">exchange rate</button> */}
       {/* </form> */}
-      <div>The  user : {JSON.stringify(data.user.login)}</div>
-      <div>The  merge : {JSON.stringify(data.merged)}</div>
+      <h1>Open Lake : Dashboard</h1>
+      <div>The user : {JSON.stringify(data.user.login)}</div>
+      <div>The merge : {JSON.stringify(data.merged)}</div>
       <div>closed or open: {JSON.stringify(data.state)}</div>
       {/* <div>The  merge : {JSON.stringify(data.merged)}</div> */}
       {/* <pre>{JSON.stringify(currency, null, 2)}</pre> */}
