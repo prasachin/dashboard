4b69175f84e770456877232b8cb300040c00e99b
sachin <sachinp@iitbhilai.ac.in>
2024-05-28 13:51:49 +0530

    codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
+
+const baseurl = "https://dashboard-be-gh5e.onrender.com/";
 
 const App = () => {
   const [data, setData] = useState(null);
@@ -7,9 +10,11 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get(`https://api.github.com/repos/Openlake/GitStartedWithUs/pulls/1`)
+      .get(baseurl)
       .then((response) => {
-        setData(response.data);
+        if (Object.keys(response.data).length !== 0) {
+          setData(response.data);
+        }
       })
       .catch((error) => {
         setError(error);
@@ -17,19 +22,80 @@ const App = () => {
   }, []);
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          color: "red",
+          borderColor: "black",
+          borderWidth: "2px",
+          borderStyle: "solid",
+        }}
+      >
+        Error: {error.message}
+      </div>
+    );
   }
 
   if (!data) {
-    return <div>Loading...</div>;
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          color: "green",
+        }}
+      >
+        Loading...
+      </div>
+    );
+  }
+  if (typeof data !== "object" || Object.keys(data).length === 0) {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          color: "red",
+        }}
+      >
+        <header className="fixed-header">
+          <h1 style={{ margin: 0 }}>Codeforge Dashboard</h1>
+        </header>
+        <h2 style={{ marginTop: "100px" }}>
+          Fetching Limit exceeded! Please try again later!
+        </h2>
+        <footer className="fixed-footer">
+          <h1 style={{ margin: 0 }}>&copy;OpenLake</h1>
+        </footer>
+      </div>
+    );
   }
 
   return (
-    <div>
-      <h1>Open Lake : Dashboard</h1>
-      <div>The user: {JSON.stringify(data.user.login, null, 2)}</div>
-      <div>The merge: {JSON.stringify(data.merged, null, 2)}</div>
-      <div>Closed or open: {JSON.stringify(data.state, null, 2)}</div>
+    <div className="container">
+      <header className="fixed-header">
+        <h1 style={{ margin: 0 }}>Codeforge Dashboard</h1>
+      </header>
+      <div className="temp">
+        <table className="leaderboard">
+          <thead>
+            <tr>
+              <th>UserName</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(data).map(([username, details]) => (
+              <tr key={username}>
+                <td>{username}</td>
+                <td>{details.points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <footer className="fixed-footer">
+        <h1 style={{ margin: 0 }}>&copy;OpenLake</h1>
+      </footer>
     </div>
   );
 };
